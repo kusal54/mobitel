da88618d2652b1406b41f20e483e52b1c9e6c80b
Kusal <kusal4978@gmail.com>
2024-09-11 08:00:53 +0530

    Publishing

---
+++ assets/js/java.js
@@ -0,0 +1,95 @@
+document.getElementById('btnCombo').onclick= function () {
+    document.getElementById('mainTable1').style.display= "none";
+    document.getElementById('mainTable2').style.display= "inherit";
+    document.getElementById('mainTable3').style.display= "none";
+    document.getElementById('mainTable4').style.display= "none";
+    document.getElementById('mainTable5').style.display= "none";
+    document.getElementById('mainTable6').style.display= "none";
+    document.getElementById('mainTable7').style.display= "none";
+    document.getElementById('mainTable8').style.display= "none";
+    document.getElementById('mainTable9').style.display= "none";
+}
+
+document.getElementById('btnMax').onclick= function () {
+    document.getElementById('mainTable1').style.display= "inherit";
+    document.getElementById('mainTable2').style.display= "none";
+    document.getElementById('mainTable3').style.display= "none";
+    document.getElementById('mainTable4').style.display= "none";
+    document.getElementById('mainTable5').style.display= "none";
+    document.getElementById('mainTable6').style.display= "none";
+    document.getElementById('mainTable7').style.display= "none";
+    document.getElementById('mainTable8').style.display= "none";
+    document.getElementById('mainTable9').style.display= "none";
+}
+
+document.getElementById('btnIndvo').onclick= function () {
+    document.getElementById('mainTable1').style.display= "none";
+    document.getElementById('mainTable2').style.display= "none";
+    document.getElementById('mainTable3').style.display= "inherit";
+    document.getElementById('mainTable4').style.display= "none";
+    document.getElementById('mainTable5').style.display= "none";
+    document.getElementById('mainTable6').style.display= "none";
+    document.getElementById('mainTable7').style.display= "none";
+    document.getElementById('mainTable8').style.display= "none";
+    document.getElementById('mainTable9').style.display= "none";
+}
+
+document.getElementById('btnTax').onclick= function () {
+    document.getElementById('mainTable1').style.display= "none";
+    document.getElementById('mainTable2').style.display= "none";
+    document.getElementById('mainTable3').style.display= "none";
+    document.getElementById('mainTable4').style.display= "inherit";
+    document.getElementById('mainTable5').style.display= "inherit";
+    document.getElementById('mainTable6').style.display= "none";
+    document.getElementById('mainTable7').style.display= "none";
+    document.getElementById('mainTable8').style.display= "none";
+    document.getElementById('mainTable9').style.display= "none";
+}
+
+document.getElementById('btn650').onclick= function () {
+    document.getElementById('mainTable1').style.display= "none";
+    document.getElementById('mainTable2').style.display= "none";
+    document.getElementById('mainTable3').style.display= "none";
+    document.getElementById('mainTable4').style.display= "none";
+    document.getElementById('mainTable5').style.display= "none";
+    document.getElementById('mainTable6').style.display= "inherit";
+    document.getElementById('mainTable7').style.display= "none";
+    document.getElementById('mainTable8').style.display= "none";
+    document.getElementById('mainTable9').style.display= "none";
+}
+
+document.getElementById('btn477').onclick= function () {
+    document.getElementById('mainTable1').style.display= "none";
+    document.getElementById('mainTable2').style.display= "none";
+    document.getElementById('mainTable3').style.display= "none";
+    document.getElementById('mainTable4').style.display= "none";
+    document.getElementById('mainTable5').style.display= "none";
+    document.getElementById('mainTable6').style.display= "none";
+    document.getElementById('mainTable7').style.display= "inherit";
+    document.getElementById('mainTable8').style.display= "none";
+    document.getElementById('mainTable9').style.display= "none";
+}
+
+document.getElementById('btn577').onclick= function () {
+    document.getElementById('mainTable1').style.display= "none";
+    document.getElementById('mainTable2').style.display= "none";
+    document.getElementById('mainTable3').style.display= "none";
+    document.getElementById('mainTable4').style.display= "none";
+    document.getElementById('mainTable5').style.display= "none";
+    document.getElementById('mainTable6').style.display= "none";
+    document.getElementById('mainTable7').style.display= "none";
+    document.getElementById('mainTable8').style.display= "inherit";
+    document.getElementById('mainTable9').style.display= "none";
+}
+
+document.getElementById('btn240').onclick= function () {
+    document.getElementById('mainTable1').style.display= "none";
+    document.getElementById('mainTable2').style.display= "none";
+    document.getElementById('mainTable3').style.display= "none";
+    document.getElementById('mainTable4').style.display= "none";
+    document.getElementById('mainTable5').style.display= "none";
+    document.getElementById('mainTable6').style.display= "none";
+    document.getElementById('mainTable7').style.display= "none";
+    document.getElementById('mainTable8').style.display= "none";
+    document.getElementById('mainTable9').style.display= "inherit";
+}
